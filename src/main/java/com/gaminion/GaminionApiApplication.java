3960e23c042f8b42d6b921cdb7e9e28fec72e161
Joyce Orog <jeicooper@users.noreply.github.com>
2026-05-11 16:28:40 +0800

    enabled scheduler

--- src/main/java/com/gaminion/GaminionApiApplication.java
+++ src/main/java/com/gaminion/GaminionApiApplication.java
@@ -2,13 +2,12 @@ package com.gaminion;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class GaminionApiApplication {
-
 	public static void main(String[] args) {
-
 		SpringApplication.run(GaminionApiApplication.class, args);
 	}
-
 }
