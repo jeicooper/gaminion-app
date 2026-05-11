b92c0de63c9755ff950953b73c11b03f0782c36d
Joyce Orog <jeicooper@users.noreply.github.com>
2026-05-11 15:06:43 +0800

    Initial commit

---
+++ src/main/java/com/gaminion/gaminion_api/GaminionApiApplication.java
@@ -0,0 +1,13 @@
+package com.gaminion.gaminion_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GaminionApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GaminionApiApplication.class, args);
+	}
+
+}
